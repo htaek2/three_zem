00076f80e3558016be88a2bb127a75389d6b0013
htaek2 <shtlrlszld@gmail.com>
2025-09-29 09:48:46 +0900

    현황 테스트

--- src/page/Condition.jsx
+++ src/page/Condition.jsx
@@ -1,4 +1,254 @@
+import React from "react";
+import "../css/condition.css";
+
 function Condition() {
-  return <div>Condition Page</div>;
+  return (
+  <>
+    <div className="title">에너지 현황</div>
+        <div className="main">
+            <div className="elect">
+                <div className="elect_1usage">
+                    <div className="elect_1usage_header">
+                        <div className="elect_1usage_header_L">
+                            <div id="elect_title"><img src="../public/elect_icon.svg" alt="전력"></img>전력</div>
+                        </div>
+                        <div className="elect_1usage_header_R">
+                            <img src="../public/energe_detail_whorf.svg" alt="분석하기"></img>
+                        </div>
+                    </div>
+                    <div className="elect_1usage_main">
+                        <div className="elect_1usage_main_L">
+                            <div className="today_title">금일 사용량</div>
+                            <div className="today_value">0,000.0 kWh</div>
+                            <div className="today_ratio">
+                                <div className="updown_icon">
+                                    <img src="../public/up_icon.svg" alt="오름세"></img>
+                                    <img src="../public/down_icon.svg" alt="내림세"></img>
+                                </div>
+                                <div>전일 동시간 대비</div>
+                                <div>00%</div>
+                                <div className="updown_font">
+                                    <div>증가</div>
+                                    <div>감소</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="elect_1usage_main_R">표</div>
+                    </div>
+                    <div className="elect_1usage_footer">
+                        <div className="elect_1usage_footer_L">
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 전일 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 1㎥ 당 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 최고 사용량</div>
+                        </div>
+                        <div className="elect_1usage_footer_R">
+                            <div>0,000.0 kWh</div>
+                            <div>0,000.0 kWh</div>
+                            <div>0,000.0 kWh</div>
+                        </div>
+                    </div>
+                </div>
+<hr className="liner"></hr>
+                <div className="elect_30usage">
+                    <div className="elect_30usage_main">
+                        <div className="elect_30usage_main_L">
+                            <div className="month_title">금월 사용량</div>
+                            <div className="month_value">0,000.0 kWh</div>
+                            <div className="month_ratio">
+                                <div className="updown_icon">
+                                    <img src="../public/up_icon.svg" alt="오름세"></img>
+                                    <img src="../public/down_icon.svg" alt="내림세"></img>
+                                </div>
+                                <div>전월 동시간 대비</div>
+                                <div>00%</div>
+                                <div className="updown_font">
+                                    <div>증가</div>
+                                    <div>감소</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="elect_30usage_main_R">표</div>
+                    </div>
+                    <div className="elect_30usage_footer">
+                        <div className="elect_30usage_footer_L">
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 전월 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 1㎥ 당 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 최고 사용량</div>
+                        </div>
+                        <div className="elect_30usage_footer_R">
+                            <div>0,000.0 kWh</div>
+                            <div>0,000.0 kWh</div>
+                            <div>0,000.0 kWh</div>
+                        </div>
+                    </div>
+                </div>
+<hr className="liner"></hr>
+                <div className="elect_charge">
+                    <div className="elect_charge_main">
+                        <div className="charge_title">전월 대비 실시간 요금</div>
+                        <div className="charge_chart">
+                            <div>표</div>
+                        </div>
+                    </div>
+                    <div className="elect_charge_footer">
+                        <div className="elect_charge_footer_L">
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 전월 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 1㎥ 당 사용량</div>
+                        </div>
+                        <div className="elect_charge_footer_R">
+                            <div>0,000 원</div>
+                            <div>0,000 원</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="gas">
+                <div className="gas_1usage">
+                    <div className="gas_1usage_header">
+                        <div className="gas_1usage_header_L">
+                            <div id="gas_title"><img src="../public/gas_icon.svg" alt="가스"></img>가스</div>
+                        </div>
+                        <div className="gas_1usage_header_R">
+                            <img src="../public/energe_detail_whorf.svg" alt="분석하기"></img>
+                        </div>
+                    </div>
+                    <div className="gas_1usage_main">
+                        <div className="gas_1usage_main_L">
+                            <div className="today_title_gas">금일 사용량</div>
+                            <div className="today_value_gas">0,000.0 ㎥</div>
+                            <div className="today_ratio_gas">
+                                <div className="updown_icon">
+                                    <img src="../public/up_icon.svg" alt="오름세"></img>
+                                    <img src="../public/down_icon.svg" alt="내림세"></img>
+                                </div>
+                                <div>전일 동시간 대비</div>
+                                <div>00%</div>
+                                <div className="updown_font">
+                                    <div>증가</div>
+                                    <div>감소</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="gas_1usage_main_R">표</div>
+                    </div>
+                    <div className="gas_1usage_footer">
+                        <div className="gas_1usage_footer_L">
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 전일 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 1㎥ 당 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 최고 사용량</div>
+                        </div>
+                        <div className="gas_1usage_footer_R">
+                            <div>0,000.0 ㎥</div>
+                            <div>0,000.0 ㎥</div>
+                            <div>0,000.0 ㎥</div>
+                        </div>
+                    </div>
+                </div>
+<hr className="liner"></hr>
+                <div className="gas_30usage">
+                    <div className="gas_30usage_main">
+                        <div className="gas_30usage_main_L">
+                            <div className="month_title_gas">금월 사용량</div>
+                            <div className="month_value_gas">0,000.0 ㎥</div>
+                            <div className="month_ratio_gas">
+                                <div className="updown_icon">
+                                    <img src="../public/up_icon.svg" alt="오름세"></img>
+                                    <img src="../public/down_icon.svg" alt="내림세"></img>
+                                </div>
+                                <div>전월 동시간 대비</div>
+                                <div>00%</div>
+                                <div className="updown_font">
+                                    <div>증가</div>
+                                    <div>감소</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="gas_30usage_main_R">표</div>
+                    </div>
+                    <div className="gas_30usage_footer">
+                        <div className="gas_30usage_footer_L">
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 전월 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 1㎥ 당 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 최고 사용량</div>
+                        </div>
+                        <div className="gas_30usage_footer_R">
+                            <div>0,000.0 ㎥</div>
+                            <div>0,000.0 ㎥</div>
+                            <div>0,000.0 ㎥</div>
+                        </div>
+                    </div>
+                </div>
+<hr className="liner"></hr>
+                <div className="gas_charge">
+                    <div className="gas_charge_main">
+                        <div className="charge_title_gas">전월 대비 실시간 요금</div>
+                        <div className="charge_chart_gas">
+                            <div>표</div>
+                        </div>
+                    </div>
+                    <div className="gas_charge_footer">
+                        <div className="gas_charge_footer_L">
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 전월 사용량</div>
+                            <div><img src="../public/condition_circle.svg" alt="⚪"></img> 1㎥ 당 사용량</div>
+                        </div>
+                        <div className="gas_charge_footer_R">
+                            <div>0,000 원</div>
+                            <div>0,000 원</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="water">
+                <div className="water_1usage">
+                    <div className="water_1usage_header">
+                        <div className="water_1usage_header_L"></div>
+                        <div className="water_1usage_header_R"></div>
+                    </div>
+                    <div className="water_1usage_main">
+                        <div className="water_1usage_main_L"></div>
+                        <div className="water_1usage_main_R"></div>
+                    </div>
+                    <div className="water_1usage_footer">
+                        <div className="water_1usage_footer_L"></div>
+                        <div className="water_1usage_footer_R"></div>
+                    </div>
+                </div>
+                <div className="water_30usage"></div>
+                <div className="water_30usage_main">
+                    <div className="water_30usage_main_L"></div>
+                    <div className="water_30usage_main_R"></div>
+                </div>
+                <div className="water_30usage_footer">
+                    <div className="water_30usage_footer_L"></div>
+                    <div className="water_30usage_footer_R"></div>
+                </div>
+                <div className="water_charge">
+                    <div className="water_charge_header"></div>
+                    <div className="water_charge_main"></div>
+                    <div className="water_charge_footer">
+                        <div className="water_charge_footer_L"></div>
+                        <div className="water_charge_footer_R"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="average">
+                <div className="average_charge">
+                    <div className="average_charge_header"></div>
+                    <div className="average_charge_main">
+                        <div className="average_charge_main_L"></div>
+                        <div className="average_charge_main_R"></div>
+                    </div>
+                    <div className="average_charge_footer">
+                        <div className="average_charge_footer_L"></div>
+                        <div className="average_charge_footer_R"></div>
+                    </div>
+                </div>
+                <div className="average_country"></div>
+                <div className="average_location"></div>
+            </div>
+        </div>
+  </>);
 }
 export default Condition;
