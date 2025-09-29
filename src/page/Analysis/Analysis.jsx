556aca74a1e14be000fd508041a180a169ef908f
MkBaek0229 <alsrl6678@naver.com>
2025-09-29 17:54:42 +0900

    통합분석페이지

--- src/page/Analysis/Analysis.jsx
+++ src/page/Analysis/Analysis.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../css/analysis.css";
 import SummaryCard from "./SummaryCard";
+import AlertBanner from "./AlertBanner";
 
 function Analysis() {
   return (
@@ -20,7 +21,10 @@ function Analysis() {
 
          <div className="analysis_Right">
             <h2>에너지 절감 방안 제시</h2>
-            <div className="alert_banner"><h3>냉방 사용량 알림</h3><span>평소 사용량보다 3% 증가하여 사용 금액이 증가 하였습니다.</span></div>
+            <AlertBanner topic="1"/>
+            <AlertBanner topic="2"/>
+            <AlertBanner topic="3"/>
+
           
         </div> 
       </main>
