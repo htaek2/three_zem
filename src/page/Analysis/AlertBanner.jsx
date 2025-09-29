8797943f28194ee1bb1c19ddcb166d56cd7c13c1
MkBaek0229 <alsrl6678@naver.com>
2025-09-29 22:56:49 +0900

    통합분석

--- src/page/Analysis/AlertBanner.jsx
+++ src/page/Analysis/AlertBanner.jsx
@@ -1,21 +1,18 @@
 function AlertBanner({ topic = "" }) {
   const iconMap = {
-    "1": "used-alert.svg",
-    "2": "solution-alert.svg",
-    "3": "pattern_error-alert.svg"
-  }
+    1: "used-alert.svg",
+    2: "solution-alert.svg",
+    3: "pattern_error-alert.svg",
+  };
 
   return (
-     <div className={`alert_banner${topic}`}>
-              <div>
-              <h3>냉방 사용량 알림</h3><span>평소 사용량보다 3% 증가하여 사용 금액이 증가 하였습니다.</span>
-              </div>
-              
-              <div>
-               {iconMap[topic] && (
-                    <img src={iconMap[topic]}/>
-        )}
-              </div>
+    <div className={`alert_banner${topic}`}>
+      <div>
+        <h3>냉방 사용량 알림</h3>
+        <span>평소 사용량보다 3% 증가하여 사용 금액이 증가 하였습니다.</span>
+      </div>
+
+      <div>{iconMap[topic] && <img src={iconMap[topic]} />}</div>
     </div>
   );
 }
--- src/page/Analysis/Analysis.jsx
+++ src/page/Analysis/Analysis.jsx
@@ -7,26 +7,23 @@ function Analysis() {
   return (
     <div>
       <div className="title">통합 분석</div>
-      <main>  
+      <main>
         <div className="analysis_Left">
           <div className="Monthly-usage-summary">
-            <SummaryCard title="월"/>
-            <SummaryCard title="일"/>
-
+            <SummaryCard title="월" />
+            <SummaryCard title="일" />
           </div>
           <section className="ranking_distribution">
             <h2>동 업종 대비 저비용 지출 분석</h2>
           </section>
         </div>
 
-         <div className="analysis_Right">
-            <h2>에너지 절감 방안 제시</h2>
-            <AlertBanner topic="1"/>
-            <AlertBanner topic="2"/>
-            <AlertBanner topic="3"/>
-
-          
-        </div> 
+        <div className="analysis_Right">
+          <h2>에너지 절감 방안 제시</h2>
+          <AlertBanner topic="1" />
+          <AlertBanner topic="2" />
+          <AlertBanner topic="3" />
+        </div>
       </main>
     </div>
   );
